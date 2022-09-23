b779e41893fbdd95cc4683004f6e1c407fd8d801
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-09-22 18:21:53 -0700

    Adding places view with mock data

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
-  })
-  
+  res.render('home')
+})
 
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.render('error404')
   })
   

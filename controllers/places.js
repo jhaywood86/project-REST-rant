5b1607f1717401269d7228079dbcf0ae5ac22ff0
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-09-25 09:52:34 -0700

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -2,22 +2,11 @@ const router = require('express').Router()
 
 
 // GET /places
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/ramen.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cool-cat.jpg'
-    }]      
-    res.render('places/index', {places})
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
 })
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')

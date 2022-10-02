05f5eb94a0aea724dd34c0cfc57dbcc9cc787c9b
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-10-02 10:47:23 -0700

    Monga connection set

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 
-
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
@@ -12,7 +11,6 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,37 +1,37 @@
+
+
 const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place, index) => {
-        return (
+    let placesFormatted = data.places.map((place) => {
+        return (            
           <div className="col-sm-6">
             <h2>
-              <a href={`/places/${index}`} >
+              <a href={`/places/${place.id}`}>
                 {place.name}
               </a>
             </h2>
-            <p className="text-center">
-              {place.cuisines}
-            </p>
-            <img src={place.pic} alt={place.name} />
-            <p className="text-center">
-              Located in {place.city}, {place.state}
-            </p>
-          </div>
+              <p className="text-center">
+                    {place.cuisines}
+              </p>
+                <img src={place.pic} alt={place.name}></img>
+              <p className="text-center">
+                Located in {place.city}, {place.state}
+              </p>
+            </div>            
         )
-      })
-      
+    })
     return (
-      <Def>
+        <Def>
           <main>
-              <h1>Places to Rant or Rave About</h1>
+            <h1>Places to Rant or Rave About</h1>
               <div className="row">
                 {placesFormatted}
               </div>
           </main>
-      </Def>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = index

3c74af071162be4ff3c05c691ee74e6bb2170c06
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-10-04 16:52:19 -0700

    helper method created

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,12 +9,23 @@ function show (data) {
                     <div>
                         <img src="/images/ramen.jpg" alt="bowl of ramen" id="ramen"/>
                     </div>
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
                     <div>
                         <h1>{ data.place.name }</h1>
                         <h2><font color="#FE8C8C">Rating</font></h2>
                         <div>currently unrated</div>
-                        <h2><font color="#FE8C8C">Comments</font></h2>
-                        <div>no comments</div>
+                        <br/>
+                        <h2><font color="#FE8C8C">Description</font></h2>
+                        <br />
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <br />
                         <div>
                             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                                 Edit
@@ -25,6 +36,13 @@ function show (data) {
                                 <button type="submit" className="btn btn-danger">
                                     Delete
                                 </button>
+                                <br/>
+                                
+                                <h2>
+                                    <font color="#FE8C8C">Comments</font>
+                                </h2>
+                                <div>no comments</div>
+                            
                             </form>  
                         </div>
                     </div>
